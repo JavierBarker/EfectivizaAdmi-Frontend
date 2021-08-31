02c639501c1b01ddc8d57cb067a847849bab48ce
Javier Barker <hernandez.javier2003@gmail.com>
2021-08-30 19:22:22 -0600

    Se creó el agregar Prestamos

--- EfectivizaAdmin/src/app/components/component1/component1.component.ts
+++ EfectivizaAdmin/src/app/components/component1/component1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-component1',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Component1Component implements OnInit {
 
-  constructor() { }
+  public step : number = 0;
+  public loanForm = this.buildFormLoad();
+  public typeForm : String = "";
+  constructor(private fmBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    const form1 = document.querySelector('#form1');
+    form1.classList.toggle('hidden');
+  }
+
+  buildFormLoad(){
+    return this.fmBuilder.group({
+      name :['', Validators.required],
+      typeLoan: ['', Validators.required]
+
+    })
+  }
+
+
+  changeForm(){
+    const form1 = document.querySelector('#form1');
+    const form2 = document.querySelector('#form2');
+
+    form1.classList.toggle('hidden');
+    form2.classList.toggle('hidden');
+  }
+
+  next(){
+    if (this.step === 1 ) {
+      this.step = 1;
+    }else{
+      this.step++
+    }
+  }
+
+  
+  checkForm(){
+    if (this.loanForm.value.typeLoan.toString() === "Mobiliario") {
+      this.typeForm = "Mobiliario";
+    }
+    if (this.loanForm.value.typeLoan.toString() === "Vehículo") {
+      this.typeForm = "Vehículo";
+    }
+    if (this.loanForm.value.typeLoan.toString() === "Joyeria") {
+      this.typeForm = "Joyeria";
+    }
+  }
+
+  previous(){
+    if (this.step === 0) {
+      this.step = 0;
+    }else{
+      this.step--
+    }
   }
 
 }
